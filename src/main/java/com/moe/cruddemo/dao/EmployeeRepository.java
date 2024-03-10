236941ecc359caeaec501714c3f29fa5c0715b87
naingminkhant <tharkhant777@gmail.com>
2024-03-10 21:01:07 +0630

    spring data rest properties

--- src/main/java/com/moe/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/moe/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.moe.cruddemo.dao;
 
 import com.moe.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
